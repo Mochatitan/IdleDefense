81749c75bda0bb30951c0adec1ad4ab64c409aa7
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-02-28 11:19:00 -0500

    Create Function.java

---
+++ src/game/Function.java
@@ -0,0 +1,16 @@
+
+
+public class Function{
+private Function(){}
+
+public static void saveGame(int saveFile){}
+  
+public static void loadGame(int saveFile){}
+
+public static String readTXT(String fileString){
+  
+return "readTXT to be made...";
+}
+
+
+}
