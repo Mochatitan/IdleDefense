bc9c71282be4ec9f59da92738e83252952f31f54
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-02-28 11:47:45 -0500

    Update Function.java

--- src/game/Function.java
+++ src/game/Function.java
@@ -3,7 +3,10 @@
 public class Function{
 private Function(){}
 
-public static void saveGame(int saveFile){}
+public static void saveGame(int saveFile){
+
+
+}
   
 public static void loadGame(int saveFile, Board board){}
 
@@ -13,9 +16,20 @@ public static int loadTest(){
 }
 
 public static String readTXT(String fileString){
-  
-return "readTXT to be made...";
+  throws IOException {
+    String str = "Hello";
+    BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+    writer.write(str);
+    
+    writer.close();
 }
+public void setInt(String fileName, int newValue) throws IOException {
+  
+    BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+    writer.write(newValue);
+    writer.close();
 
+
+}
 
 }
