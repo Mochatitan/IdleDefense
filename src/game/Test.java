aee598709af0fd349b563e8c5cbfe660e2530f8e
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2023-02-28 13:43:11 -0500

    Update Test.java

--- src/game/Test.java
+++ src/game/Test.java
@@ -4,7 +4,20 @@ public class Test{
 private Test(){}
 public int Monee;
 public static void main(String[] args){
-  Function.loadTest(Monee);
+   testingMethod();
+}
+  
+public void testingMethod() 
+  throws IOException {
+    String str = "Hello";
+    String fileName = "src/game/saves/Save1.txt"
+    Path path = Paths.get(fileName);
+    byte[] strToBytes = str.getBytes();
+
+    Files.write(path, strToBytes);
+
+    String read = Files.readAllLines(path).get(0);
+    assertEquals(str, read);
 }
   
 }
